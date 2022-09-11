89b26a9b762bf00948df4cdb25dadda95b509dfa
Dmitry Bogatyrev <bogatyrov200488@gmail.com>
2022-09-11 16:35:09 +0500

    f

--- worker/main.py
+++ worker/main.py
@@ -237,7 +237,9 @@ class BtWorker():
                 #     print('sssss23', s.split('='))
                 tmp = dict(s.split("=") for s in tempData.split(","))
                 obj = dict()
-                obj['temp'] = tmp["T"]
+
+                obj['temp'] = tmp["T"] if 'T' in tmp else ""
+
                 # obj['temp'] = tmp["T"] ? tmp['T']:""
                 obj['h2'] = tmp["H2"].replace("ppm", "")
                 # obj['maxH2']= float(tmp["H2"].replace("ppm", ""))
