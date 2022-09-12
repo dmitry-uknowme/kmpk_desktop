6161b542fefc709664e151d7bffa813ce7a9a358
Dmitry Bogatyrev <bogatyrov200488@gmail.com>
2022-09-12 19:06:49 +0500

    f

--- Run.bat
+++ Run.bat
@@ -1,1 +1,1 @@
-concurrently "cd C:\app\kmpk_desktop\ui && npm run dev" "npx kill-port 8081 && cd C:\app\kmpk_desktop\server && npm run dev" "start C:\app\kmpk_desktop\Run.bat"
+concurrently "cd C:\app\kmpk_desktop\ui && npm run dev" "npx kill-port 8081 && cd C:\app\kmpk_desktop\server && npm run dev" "start C:\app\kmpk_desktop\RunWorker.bat"
